af63e197dfdc6c98124c30d2958272c487c0fe42
wei120698598 <120698598@qq.com>
2015-07-07 23:46:50 +0800

    Revert "应用商店"
    This reverts commit e03931ce87550a9db2f7651e53e6a12c090a1ed4.

--- gen/com/aaa/gomarket/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.aaa.gomarket;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/aaa/gomarket/BaseActivity.java
+++
@@ -1,7 +0,0 @@
-package com.aaa.gomarket;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class BaseActivity extends AppCompatActivity {
-
-}
